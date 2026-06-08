d957d3c9eb7187b04317c28e5be00439356b2527
mirellamottaaraujo-sys <mirella.motta.araujo@escola.pr.gov.br>
2026-06-08 08:24:55 -0300

    Update script.js
    kk

--- script.js
+++ script.js
@@ -1,51 +1,15 @@
-body{
-    margin:0;
-    font-family:Arial, sans-serif;
-    background:#f4f4f4;
-}
+function mostrarMensagem() {
 
-header{
-    background:#2e7d32;
-    color:white;
-    text-align:center;
-    padding:40px;
-}
+    let mensagens = [
+        "🌱 A agricultura é essencial para a vida.",
+        "🌎 Preserve o meio ambiente para um futuro melhor.",
+        "🚜 O campo e a cidade dependem um do outro.",
+        "💧 Economizar água é um ato de cidadania.",
+        "🌻 Sustentabilidade é cuidar do presente e do futuro."
+    ];
 
-section{
-    background:white;
-    margin:20px;
-    padding:20px;
-    border-radius:10px;
-    box-shadow:0 0 10px rgba(0,0,0,0.1);
-}
+    let sorteio = Math.floor(Math.random() * mensagens.length);
 
-h2{
-    color:#2e7d32;
-}
-
-button{
-    background:#4caf50;
-    color:white;
-    border:none;
-    padding:12px 20px;
-    border-radius:5px;
-    cursor:pointer;
-    font-size:16px;
-}
-
-button:hover{
-    background:#1b5e20;
-}
-
-#mensagem{
-    margin-top:15px;
-    font-weight:bold;
-    color:#1b5e20;
-}
-
-footer{
-    background:#1b5e20;
-    color:white;
-    text-align:center;
-    padding:20px;
+    document.getElementById("mensagem").innerHTML =
+        mensagens[sorteio];
 }
